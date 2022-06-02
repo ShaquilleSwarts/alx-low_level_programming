b753cbef4c7ad1ddc53c9761b4fc99f6180815d1
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-02 10:15:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,6 @@
 #include <string.h>
 int main(void)
 {
-char *str1;
-strcpy(str1, "\"Programming is like building a multilingual puzzle");
-puts(str1);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
