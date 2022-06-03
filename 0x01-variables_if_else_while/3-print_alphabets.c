5b2e079d335843a8f19daef9f8dbe32be693ca5d
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-03 11:08:16 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-char c; 
+char c;
 char A;
-for (c = 'a'; c <= 'z'; ++c) 
+for (c = 'a'; c <= 'z'; ++c)
 {
 putchar(c);
 }
 for (A = 'A'; A <= 'Z'; ++A)
-{  
+{
 putchar(A);
 }
 putchar('\n');
