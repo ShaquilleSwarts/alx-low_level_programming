dc166556c10e60951cd818fcab9c53e699923880
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-03 08:50:46 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,13 +13,13 @@ int n;
 int m;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-m = n%10;
+m = n % 10;
  printf("last digit of %d is %d", n,m);
 if (m > 5)
-printf("and is greater than 5\n");
+printf(" and is greater than 5\n");
 else if (m == 0)
-printf("and is 0\n");
+printf(" and is 0\n");
 else if (m > 6)
-printf("and is less than 6 and not 0\n");
+printf(" and is less than 6 and not 0\n");
 return (0);
 }
