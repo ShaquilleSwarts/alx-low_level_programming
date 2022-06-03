d05e0ef44915a862ba729dcadfe46b78f17a6bb9
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-03 11:38:38 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - find positive or negative
+ * Description: using random number generator
+ * Return: Returns a 0
+ */
+int main(void)
+{
+char c;
+char A;
+for (c = 48; c <= 57; ++c)
+{
+putchar(c);
+}
+for (A = 'a'; A <= 'f'; ++A)
+{
+putchar(A);
+}
+putchar('\n');
+return (0);
+}
