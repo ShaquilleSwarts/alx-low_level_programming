0483cb403f65cb5112676f631e8074edd2fbd7cd
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-03 11:31:13 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - find positive or negative
+ * Description: using random number generator
+ * Return: Returns a 0
+ */
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; --c)
+putchar(c);
+putchar('\n');
+return (0);
+}
