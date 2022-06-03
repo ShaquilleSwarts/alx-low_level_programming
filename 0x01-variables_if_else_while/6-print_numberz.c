481f30f7492582df44cb0391d9ea90a838ed98cd
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-03 11:28:01 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - find positive or negative
+ * Description: using random number generator
+ * Return: Returns a 0
+ */
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; ++c)
+putchar(c);
+putchar('\n');
+return (0);
+}
