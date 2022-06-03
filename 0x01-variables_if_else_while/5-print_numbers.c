a231ea2b3e57e6b39be5c65a31ab7ff4f32b048e
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-03 11:23:17 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - find positive or negative
+ * Description: using random number generator
+ * Return: Returns a 0
+ */
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; ++c)
+printf("%d", c);
+printf("\n");
+return (0);
+}
