4dea67f689baf64fae1b7eb0cddfa382ec39ae5e
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-07 07:50:04 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,14 @@
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <stdio.h>
-/**
- * main - print char
- * Description: printing with new lines
- * Return: Returns a 0
- */
-int main(void)
+#include "main.h"
+int main()
 {
-putchar(95);
-putchar(112);
-putchar(117);
-putchar(116);
-putchar(99);
-putchar(104);
-putchar(97);
-putchar(114);
-putchar('\n');
+  _putchar (95);
+  _putchar (112);
+  _putchar (117);
+  _putchar (116);
+  _putchar (99);
+  _putchar (104);
+  _putchar (97);
+  _putchar (114);
 return (0);
 }
+
