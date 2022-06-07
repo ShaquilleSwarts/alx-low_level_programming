4cf3f421858ca177ed52ef896f87e18b0f73e985
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-07 12:48:52 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * int print_sign - is a sign
+ * print_sign - is a sign
  * Description: is a pos or neg
- * @ n: n is int  
+ * @n: n is int
  * Return: Returns a 0,1 or -1
  */
 int print_sign(int n)
@@ -12,7 +12,7 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-if(n < 0 )
+if (n < 0)
 {
 _putchar('-');
 return (-1);
