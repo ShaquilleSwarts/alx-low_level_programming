b3a13ad40d76d69b76d467267f725f3c56e130b2
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-07 12:58:58 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - calc abs val
+ * Description: is calc
+ * @n: n is int
+ * Return: Returns aboslute value
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+}
+return (n);
+}
