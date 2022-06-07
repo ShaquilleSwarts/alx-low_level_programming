21e1f6b51f0a31b88f3ee3ac8e59109119c252f5
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-07 04:32:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+File Edit Options Buffers Tools C Help
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - print char
+ * Description: printing with new lines
+ * Return: Returns a 0
+ */
+int main(void)
+char c = "_putchar"
+{
+char c;
+
+putchar(c);
+putchar('\n');
+return (0);
+}
