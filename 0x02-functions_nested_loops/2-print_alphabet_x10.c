1b1fa3236cb833a3910bb8e6f49638b0d1b49e3b
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-07 09:52:39 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,10 @@ int print_alphabet_x10(void)
 {
 char c;
 int a;
-for ( a = 0; a < 10; ++a){
-for (c = 'a'; c <= 'z'; ++c){
+for (a = 0; a < 10; ++a)
+{
+for (c = 'a'; c <= 'z'; ++c)
+{
 _putchar(c);
 }
 _putchar('\n');
