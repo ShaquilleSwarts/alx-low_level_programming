7980cbe64da8194c8c2cfd61e5fd122159e72b3f
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-10 06:44:28 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies int
+ * Description: multiplies int
+ * @a: param is a
+ * @b: param is b
+ * Return: Returns 0
+ */
+
+int mul(int a, int b)
+
+{
+int x;
+
+x = a * b;
+
+return (x);
+}
