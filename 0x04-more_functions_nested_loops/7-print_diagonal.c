2f5469e80d83ad4272fe8a0e580d32574484e9bf
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-10 11:28:58 -0700

    7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - prints forward slash
+ * Description: prints slash n times 
+ * @n: param is number of loops
+ * Return: Returns 0
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+  
+for ( i = 0 ; i < n; ++i)
+{
+for (j = 0; j < i; ++j)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+}
