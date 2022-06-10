9c4575dfd3fe32e717d9f46f9d6f8ce898df6116
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-10 11:10:23 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_line - prints lines 
+ * print_line - prints lines
  * Description: prints lines n times
  * @n: param is number of loops
  * Return: Returns 0
@@ -8,7 +8,7 @@
 void print_line(int n)
 {
 int i;
-for(i = 0; i < n; ++i)
+for (i = 0; i < n; ++i)
 {
 _putchar(95);
 }
