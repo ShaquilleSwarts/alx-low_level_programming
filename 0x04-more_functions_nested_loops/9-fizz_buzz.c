28b74c95d81cacef48eb6ef8fa2ba1ad5fbcce18
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-10 12:04:21 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints fizzbuzz
+ * Description: prints fizz or buzz
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; ++i)
+{
+if (i % 3 == 0)
+printf("Fizz");
+if (i % 5 == 0)
+printf("Buzz");
+if ((i % 3 != 0) && (i % 5 != 0))
+printf("%d", i);
+printf(" ");
+}
+
+return (0);
+}
