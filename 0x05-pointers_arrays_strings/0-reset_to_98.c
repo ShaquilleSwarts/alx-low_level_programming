a170a7abac38344c5d8a4d8f0c13c4704a357487
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-13 11:47:45 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets value
+ * Description: reset value to 98
+ * @n: param is n
+ * Return: Returns 0
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
