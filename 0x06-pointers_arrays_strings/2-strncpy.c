6427579595ec18dcafa91cf61806c13f7738c677
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-06-16 09:18:16 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * _strncpy - copies string
+ * Description: copy string
+ * @dest: param is dest
+ * @src: param is src
+ * @n: param is n
+ * Return: Returns 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int c = 0;
+
+while (src[c] != '\0' && c < n)
+{
+dest[c] = src[c];
+c++;
+}
+dest[c] = '\0';
+return (dest);
+}
