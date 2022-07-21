de74c618f9b5199956797488ba002fd210c90722
Shaquille Swarts <ShaquilleSwarts12@gmail.com>
2022-07-21 04:41:06 -0700

    betty checks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 #include <stddef.h>
 /**
- * unsigned int binary_to_uint- converts binary
+ * binary_to_uint- converts binary
  * @b:b is param
  * Return:converted number
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int val = 0;
-  int i = 0;
+unsigned int val = 0;
+int i = 0;
 
-  if (b == NULL)
-    return 0;
+if (b == NULL)
+return (0);
 
-  while (b[i] == '0' || b[i] == '1')
-    {
-      val <<= 1;
-      val += b[i]-'0';
-      i++;
-    }
-  return val;
+while (b[i] == '0' || b[i] == '1')
+{
+val <<= 1;
+val += b[i] - '0';
+i++;
+}
+return (val);
 }
